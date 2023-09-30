ac79f9b4c8389424353b6732e7a68bc2ccc51bbc
babitaism <rbabita044@gmail.com>
2023-09-30 13:41:11 +0530

    server changes removed master checkout

--- startServer.sh
+++ startServer.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-git clone  "https://github.com/Babitaism/Wine-data-set.git" && cd Wine-data-set && git checkout master && yarn install && yarn start
+git clone  "https://github.com/Babitaism/Wine-data-set.git" && cd Wine-data-set  && yarn install && yarn start
